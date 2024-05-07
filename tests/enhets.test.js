6dc1b61d79c34ee77f22e8697cf6da77a18d945a
tareHej <128603982+tareHej@users.noreply.github.com>
2024-05-07 15:54:53 +0200

    åtgärdad enhets.test.js

--- tests/enhets.test.js
+++ tests/enhets.test.js
@@ -1,5 +1,5 @@
 const stack = require('../src/stack');
 
 test('pop on a empty stack returns null', () => {
-    expect(stack.pop()).toBeNull(); //förväntar "null", kommer få "undefined"
+    expect(stack.pop()).toBeUndefined(); //förväntar undefined
 });
