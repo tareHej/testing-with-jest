3f49841a238b8c920d29b0cfcddfa319a7cb9798
tareHej <128603982+tareHej@users.noreply.github.com>
2024-05-23 12:38:12 +0200

    fix för seleniumtest

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ window.onload = function () {
     var peek = document.getElementById('peek');
     var display = document.getElementById('top_of_stack');
 
+    display.innerHTML = "n/a";
+
     pop.addEventListener("click", function() {
         var text = "Tog bort " + stack.pop();
         alert(text);
@@ -15,8 +17,10 @@ window.onload = function () {
 
     push.addEventListener("click", function() {
         var x = prompt("Vad ska vi lägga på stacken?");
-        stack.push(x);
-        display.innerHTML = x;
+        if (x !== null) {
+            stack.push(x);
+            display.innerHTML = x;
+        }
     });
 
     peek.addEventListener("click", function() {
